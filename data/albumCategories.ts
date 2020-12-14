2fbd2afb2d2955b6ad0b005588b9c5541348fdde
Ali.S <47412207+archsmilson@users.noreply.github.com>
2020-12-14 22:35:16 +0100

    init

---
+++ data/albumCategories.ts
@@ -0,0 +1,67 @@
+export default [{
+  id: '1',
+  title: 'Good',
+  albums: [
+    {
+      id: "1",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "2",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "3",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+
+  ]
+},
+{
+  id: '2',
+  title: 'Still Good',
+  albums: [
+    {
+      id: "1",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "2",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "3",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+
+  ]
+},
+{
+  id: '3',
+  title: 'Still still Good',
+  albums: [
+    {
+      id: "1",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "2",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+    {
+      id: "3",
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      artistHeadline: 'John Frusciante, RHCP, Pink Floyd'
+    },
+
+  ]
+},
+];
---
+++ data/albumDetails.ts
@@ -0,0 +1,38 @@
+export default {
+
+    id: "1",
+    name: "namo",
+    by: "smoko",
+    numberOfLikes: 10000,
+    imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+    artistHeadline: 'Snow, Mlow, Flow, Go',
+
+  songs: [
+    {
+      id: 1,
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      title: "Titelloo",
+      artist: "Mickelangelo"
+    },
+
+    {
+      id: 2,
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      title: "Titelloo",
+      artist: "Mickelangelo"
+    },
+    {
+      id: 3,
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      title: "Titelloo",
+      artist: "Mickelangelo"
+    },
+    {
+      id: 4,
+      imageUri: 'https://mavciefendi.files.wordpress.com/2014/04/fikret-kizilok-asik-veysel.jpg?w=590',
+      title: "Titelloo",
+      artist: "Mickelangelo"
+    }
+  ]
+
+};
